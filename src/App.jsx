32c8ad90cedf03738ea96f55b67d2d471bc1c105
Osama Al-Mangathi <osamah2587@gmail.com>
2023-01-11 13:13:16 +0000

    completing the hoobank website

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
 import styles from "./style";
-
 import {
-  Navbar,
-  Hero,
-  Stats,
-  Business,
   Billing,
+  Business,
   CardDeal,
-  Testimonials,
   Clients,
   CTA,
   Footer,
+  Navbar,
+  Stats,
+  Testimonials,
+  Hero,
 } from "./components";
 
 const App = () => (
-  <div className=" bg-primary w-full overflow-hidden">
+  <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
 
-    <div calssName={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth} mx-auto`}>
-        {/* there is no "mx-auto" I just added it to make the div in the center */}
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
     </div>
 
-    <div calssName={`bg-primary ${styles.flexStart}`}>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
